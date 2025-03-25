6648548746f05c06639671b626221636aca48f3b
nitishshukla86 <56676605+nitishshukla86@users.noreply.github.com>
2025-03-25 21:04:23 +0000

    Deploying to gh-pages from @ nitishshukla86/nitishshukla86.github.io@7637f6d85486db5443729d66f4ba548c93e7390b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -408,16 +408,16 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
+            },},{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-paper-titled-face-demorphing-via-identity-preserving-image-decomposition-accepted-at-ieee-ijcb-2024",
+          title: 'Paper titled “Face Demorphing via Identity Preserving Image Decomposition” accepted at IEEE IJCB...',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
