9c6962f9800504d6fd31db82f7a56d2bdfa8624e
nitishshukla86 <56676605+nitishshukla86@users.noreply.github.com>
2025-03-26 01:03:54 +0000

    Deploying to gh-pages from @ nitishshukla86/nitishshukla86.github.io@e026fb310af0125edc829a5098d480432dd6d8fb 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -467,14 +467,21 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%73%68%75%6B%6C%61%6E%33@%6D%73%75.%65%64%75", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-instagram',
+        title: 'Instagram',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://instagram.com/_the_doctor_chef_", "_blank");
+        },
+      },{
+        id: 'social-linkedin',
+        title: 'LinkedIn',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.linkedin.com/in/shuklanitish", "_blank");
         },
       },{
         id: 'social-rss',
@@ -488,14 +495,7 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://scholar.google.com/citations?user=pAedm2oAAAAJ", "_blank");
         },
       },{
       id: 'light-theme',
