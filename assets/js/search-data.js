c1dec52009a2673c4aa16ec0eec4c62ed2e3bf2f
nitishshukla86 <56676605+nitishshukla86@users.noreply.github.com>
2025-09-21 23:44:16 +0000

    Deploying to gh-pages from @ nitishshukla86/nitishshukla86.github.io@d72727310dd95920189c3d84d038d3b9f7448514 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,8 +16,8 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "nav-",
-          title: "",
+        },{id: "nav-projects",
+          title: "projects",
           description: "",
           section: "Navigation",
           handler: () => {
